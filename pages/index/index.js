195ce2215dec93167ee151562cf1b6089fa05b21
l-tmy <18332566714@163.com>
2018-09-29 17:54:56 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    proList:[1,2,3,4,5,6]
+  },
+  onLoad: function(){
+    this.setData({
+      test: '01'
+    })
+  },
+  toDetail:function(e){
+    console.log(e);
+    var index = e.currentTarget.dataset.index;
+    console.log(index)
+  }
+})
